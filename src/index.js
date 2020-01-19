727d7aa94f98ed0d7b5e0536f2749885b99eb061
user <irchello@gmail.com>
2020-01-18 21:54:43 -1000

    Master branch commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import App from './App';
+import 'react-bulma-components/dist/react-bulma-components.min.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
